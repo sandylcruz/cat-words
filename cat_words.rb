5b0bd7c6bfa4de65ff3c8f2cf64a31348fa8f203
Sandy Cruz <sandyleahcruz@icloud.com>
2020-04-04 15:43:28 -0700

    Migrate to complete dictionary

--- cat_words.rb
+++ cat_words.rb
@@ -5,8 +5,17 @@ require 'csv'
 # Print welcome message
 puts 'Welcome to cat words!'
 
-# Load dictionary as a hash
-raw_dictionary = CSV.read('dictionary.csv', headers: true)
+def load_raw_dictionary
+  csv_read_options = { headers: true, skip_blanks: true }
+  ('A'..'Z').each_with_object([]) do |letter, accumulator|
+    path = "dictionary/#{letter}.csv"
+    dictionary_for_letter = CSV.read(path, **csv_read_options)
+    dictionary_for_letter.each do |row|
+      accumulator.push(row.to_hash)
+    end
+    accumulator
+  end
+end
 
 def create_dictionary_as_hash(dictionary_as_array)
   dictionary_as_hash = Hash.new { |hash, key| hash[key] = [] }
@@ -18,6 +27,12 @@ def create_dictionary_as_hash(dictionary_as_array)
     accumulator
   end
 end
+
+puts 'Loading dictionary...'
+# Load dictionary as a hash
+raw_dictionary = load_raw_dictionary
+
+puts 'Dictionary loaded!'
 
 dictionary_as_hash = create_dictionary_as_hash(raw_dictionary)
 
